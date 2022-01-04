8d04e8a69f84d684f4772dd869e65446e559c42d
Lucas <lucas.tuozzo@mediamonks.com>
2022-01-04 13:53:52 -0300

    Add navigation images.

--- components/wrapper/Navigation.tsx
+++ components/wrapper/Navigation.tsx
@@ -1,10 +1,22 @@
 import classes from './Navigation.module.css';
-import {Fragment} from "react";
 
 function Navigation(props: any) {
     return (
         <header >
-
+            <img src='/svg/motogp-logo.svg' className={classes.motogpLogo} />
+            <div>
+                <p>MotoGp FANS</p>
+                <p>MOTORCYCLE BRANDS</p>
+                <p>eSPORTS</p>
+                <p>BLACK FRIDAY 2020</p>
+            </div>
+            <div>
+                <img src='/svg/search.svg' className={classes.search} />
+                <img src='/svg/favorite.svg' className={classes.favorite} />
+                <img src='/svg/helmet.svg' className={classes.helmet} />
+                <img src='/svg/cart.svg' className={classes.cart} />
+                <img src='/svg/logo-icon-white.svg' className={classes.logoIcon} />
+            </div>
         </header>
     );
 }
