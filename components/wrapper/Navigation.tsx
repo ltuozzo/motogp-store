679378c39fbd408bfe7e42bdf15e2a3d5b7675f5
Lucas <lucas.tuozzo@mediamonks.com>
2022-01-05 16:59:25 -0300

    Add navigation styles.

--- components/wrapper/Navigation.tsx
+++ components/wrapper/Navigation.tsx
@@ -2,15 +2,15 @@ import classes from './Navigation.module.css';
 
 function Navigation(props: any) {
     return (
-        <header >
+        <header className={classes.navigation}>
             <img src='/svg/motogp-logo.svg' className={classes.motogpLogo} />
-            <div>
+            <div className={classes.menu}>
                 <p>MotoGp FANS</p>
                 <p>MOTORCYCLE BRANDS</p>
                 <p>eSPORTS</p>
                 <p>BLACK FRIDAY 2020</p>
             </div>
-            <div>
+            <div className={classes.icons}>
                 <img src='/svg/search.svg' className={classes.search} />
                 <img src='/svg/favorite.svg' className={classes.favorite} />
                 <img src='/svg/helmet.svg' className={classes.helmet} />
